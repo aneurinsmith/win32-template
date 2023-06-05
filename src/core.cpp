db48774e36600e2066ae32b90235ebdba3782087
Aneurin Smith <aneurinsmith@yahoo.com>
2023-06-05 19:47:13 +0100

    Removed old implementation of dock to be readded later.

--- src/core.cpp
+++ src/core.cpp
@@ -2,23 +2,44 @@
 #include "core.h"
 #include "win32/window/window.h"
 
+
 Core::Core() {
-	params.load();
+	//params.load();
+
+	string appdata = get_appdata();
+	appdata.append("\\test.ini");
+	File f;
+	f.load(appdata);
 }
 
 void Core::save() {
-	params.save(_window->hwnd);
+	//params.save(_window->hwnd);
 }
 
 LRESULT Core::init() {
+
+	//LOG::console_logger();
 	_caption = &caption;
 	caption.init(_window->hwnd);
 
+	//_dock = &dock;
+	//dock.init(_window->hwnd);
+
+	/*
 	_panel1 = &panel1;
 	panel1.init(_window->hwnd);
+	dock.add({ 0, 0 }, _panel1->hwnd);
 
+	
 	_panel2 = &panel2;
 	panel2.init(_window->hwnd);
+	dock.add({ 3, 10 }, _panel2->hwnd);
+
+	_panel3 = &panel3;
+	panel3.init(_window->hwnd);
+	dock.add({ 1, 10 }, _panel3->hwnd);
+	*/
+
 	return true;
 }
 
@@ -27,22 +48,5 @@ int MARGIN_Y = 28;
 
 void Core::handle_resize() {
 	caption.resize({ 0,1,_window->rgn.right,_window->rgn.top-1 }, NULL);
-
-	if (panel1.docked) {
-		panel1.resize({
-			MARGIN_X,
-			(_window->rgn.top) + MARGIN_X,
-			(_window->rgn.right / 2) - MARGIN_X,
-			(_window->rgn.bottom) - (_window->rgn.top) - MARGIN_Y
-		}, NULL);
-	}
-
-	if (panel2.docked) {
-		panel2.resize({
-			(_window->rgn.right / 2) + MARGIN_X,
-			(_window->rgn.top) + MARGIN_X,
-			(_window->rgn.right) - (_window->rgn.right / 2) - (MARGIN_X * 2),
-			(_window->rgn.bottom) - (_window->rgn.top) - MARGIN_Y
-		}, NULL);
-	}
+	dock.resize({ 3,104,_window->rgn.right-6, _window->rgn.bottom-127 }, NULL);
 }
--- src/core.h
+++ src/core.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "include.h"
-#include "tools/dock/dock.h"
-#include "tools/params/params.h"
+#include "tools/file/file.h"
+//#include "tools/params/params.h"
 
+#include "win32/dock/dock.h"
 #include "win32/panel/panel.h"
 #include "win32/caption/caption.h"
+
 
 
 class Window;
@@ -22,12 +24,15 @@ public:
 	LRESULT init();
 
 private:
-	Params params;
+	//Params params;
 
 	Window* _window = nullptr;
 
 	Caption caption;
 	Caption* _caption = nullptr;
+
+	Dock dock;
+	Dock* _dock = nullptr;
 
 	Panel panel1;
 	Panel* _panel1 = nullptr;
@@ -35,7 +40,11 @@ private:
 	Panel panel2;
 	Panel* _panel2 = nullptr;
 
+	Panel panel3;
+	Panel* _panel3 = nullptr;
+
 
 protected:
 	void handle_resize();
 };
+
