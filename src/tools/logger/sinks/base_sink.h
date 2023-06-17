9358f937685d0f2788d684df469175281f8adc27
Aneurin Smith <aneurinsmith@yahoo.com>
2023-06-17 01:13:43 +0100

    Moved generic variables to base_sink class to reduce lines in future.

--- src/tools/logger/sinks/base_sink.h
+++ src/tools/logger/sinks/base_sink.h
@@ -4,14 +4,21 @@
 	\author		Aneurin F. Smith
 */
 
+#pragma once
 #include "include.h"
 
 enum Level:int;
 class basesink {
 
 public:
+
 	~basesink() = default;
 	virtual void set_level(const Level) = 0;
 	virtual void print(const Level, const string) = 0;
+
+protected:
+	string name;
+	int ringbuffer;
+	Level min_lvl;
 
 };
